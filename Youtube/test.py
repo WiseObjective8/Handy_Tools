57da4d1178ba0a6c97446e0660a10f123f4049e2
WiseObjective8 <89731846+WiseObjective8@users.noreply.github.com>
2024-08-29 01:14:51 +0530

    tests YT and PL classes

---
+++ Youtube/test.py
@@ -0,0 +1,8 @@
+'''Test file'''
+import Youtube.yt_playlist_logic as yt_playlist_logic
+import Youtube.yt_vid_logic as yt_vid_logic
+
+# yt_playlist_logic.PL(
+#     url="https://www.youtube.com/playlist?list=PLjp0AEEJ0-fGi7bkjrGhBLUF9NMraL9cL"
+# ).download_playlist()
+# yt_vid_logic.YT(url="https://youtu.be/pu9FP6xjMbQ").download_video(3, 1)
